ed4a1d842a5d9f3e578fae0f5bb8c11660564cc8
AbhiAndure02 <abhiandure123@gmail.com>
2024-01-04 23:56:18 +0530

    first commit

---
+++ CalculateAreaOfCircle.java
@@ -0,0 +1,11 @@
+public class CalculateAreaOfCircle {
+
+    public static void main(String[] args) {
+        int redius =6;
+
+        double area = 3.14 * redius*redius;
+
+        System.out.println(area);
+
+    }
+}
