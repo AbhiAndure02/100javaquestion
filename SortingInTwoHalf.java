8ad6239e49e10c7e82f15fd28dbd1f948e25cfc2
AbhiAndure02 <abhiandure123@gmail.com>
2024-01-05 11:09:45 +0530

    first commit

---
+++ SortingInTwoHalf.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class SortingInTwoHalf {
+    static void printOrder(int[] arr, int n)
+    {
+
+        Arrays.sort(arr);
+
+
+        for (int i = 0; i < n / 2; i++)
+        {
+            System.out.print(arr[i] + " ");
+        }
+
+        for (int j = n - 1; j >= n / 2; j--)
+        {
+            System.out.print(arr[j] + " ");
+        }
+
+    }
+
+
+
+    public static void main(String[] args) {
+        int[] arr = {3, 2, 4, 1, 10, 30, 40, 20};
+        int n = arr.length;
+        printOrder(arr, n);
+
+    }
+}
